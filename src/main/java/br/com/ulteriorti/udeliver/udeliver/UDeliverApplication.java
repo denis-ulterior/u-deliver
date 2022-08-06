11c7036f2f6a1e0a006d9613218d514b490d3ac6
Denis Souza <denis.souza@ulteriorti.com.br>
2022-08-06 20:49:44 -0300

    Initial commit

---
+++ src/main/java/br/com/ulteriorti/udeliver/udeliver/UDeliverApplication.java
@@ -0,0 +1,13 @@
+package br.com.ulteriorti.udeliver.udeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UDeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ulteriorti/udeliver/udeliver/UDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ulteriorti.udeliver.udeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
