983f43bbc4c225028400c2cdcb1af6438481c7b1
Denis Souza <denis.souza@ulteriorti.com.br>
2022-08-07 16:52:32 -0300

    POST /orders ok

--- src/main/java/br/com/ulteriorti/udeliver/udeliver/controllers/OrderController.java
+++ src/main/java/br/com/ulteriorti/udeliver/udeliver/controllers/OrderController.java
@@ -1,16 +1,14 @@
 package br.com.ulteriorti.udeliver.udeliver.controllers;
 
 import br.com.ulteriorti.udeliver.udeliver.dto.OrderDTO;
-import br.com.ulteriorti.udeliver.udeliver.dto.ProductDTO;
-import br.com.ulteriorti.udeliver.udeliver.entities.Order;
 import br.com.ulteriorti.udeliver.udeliver.services.OrderService;
-import br.com.ulteriorti.udeliver.udeliver.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.Servlet;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,8 +19,15 @@ public class OrderController {
     private OrderService service;
 
     @GetMapping
-    public ResponseEntity<List<OrderDTO>> findAll(){
+    public ResponseEntity<List<OrderDTO>> findAll() {
         List<OrderDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
--- src/main/java/br/com/ulteriorti/udeliver/udeliver/services/OrderService.java
+++ src/main/java/br/com/ulteriorti/udeliver/udeliver/services/OrderService.java
@@ -1,12 +1,17 @@
 package br.com.ulteriorti.udeliver.udeliver.services;
 
 import br.com.ulteriorti.udeliver.udeliver.dto.OrderDTO;
+import br.com.ulteriorti.udeliver.udeliver.dto.ProductDTO;
 import br.com.ulteriorti.udeliver.udeliver.entities.Order;
+import br.com.ulteriorti.udeliver.udeliver.entities.OrderStatus;
+import br.com.ulteriorti.udeliver.udeliver.entities.Product;
 import br.com.ulteriorti.udeliver.udeliver.repositories.OrderRepository;
+import br.com.ulteriorti.udeliver.udeliver.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +19,23 @@ import java.util.stream.Collectors;
 public class OrderService {
     @Autowired
     private OrderRepository repository;
+
+    @Autowired
+    private ProductRepository productRepository;
     @Transactional(readOnly = true)
-    public List<OrderDTO> findAll(){
+    public List<OrderDTO> findAll() {
         List<Order> list = repository.findOrdersWithProducts();
-        return list.stream().map(item ->new OrderDTO(item)).collect(Collectors.toList());
+        return list.stream().map(item -> new OrderDTO(item)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+        for (ProductDTO p : dto.getProducts()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        order = repository.save(order);
+        return new OrderDTO(order);
     }
 }
